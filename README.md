240a9cece7c0625b71d935511a32879c9a30727e
Nadid Wasique <76746289+DaTaklaCatto@users.noreply.github.com>
2022-12-03 16:16:50 +0000

    Added Initial Test Bot Index

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Discord.JS-V14-Commands-Handler
+# Discord.JS V14 Commands Handler
 An amazing and advanced command handler for discord.js version-14. Legend: Commands, Events, Permissions, MongoDB and More! - by Nadid Wasique [DaTaklaCatto]
---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const client = new Client({
+    allowedMentions: {
+    parse: ["roles", "users", "everyone"],
+    repliedUser: false,
+  },
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
+});
+
+client.login(process.env.TOKEN).catch((error) => {
+  console.log("LMAO I COULDN'T LOGIN")
+});
